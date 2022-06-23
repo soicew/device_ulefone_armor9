ce9ba3485e4f82f273c7e3cfc6f6556ec61dcd47
vik0t0r <victordpa7@gmail.com>
2022-06-23 13:47:18 +0200

    why

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,4 +9,5 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
+TARGET_ARCH := arm64
 -include vendor/ulefone/armor9/BoardConfigVendor.mk
